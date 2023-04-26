60f576ca579e4f033c98fc72f0db94b94f40a612
Rahul <rahulmishra4317@gmial.com>
2023-04-26 22:30:16 +0530

    player wala assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,18 +3,52 @@ const lodash = require('lodash')
 var bodyParser = require('body-parser');
 const { route } = require('express/lib/application');
 const router = express.Router();
+let array =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ]
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ]
+        },
+    ]
+
+
 
 
 router.get('/test-me', function (req, res) {  //question number 1
-    let array = [1, 2, 3, 5, 6, 7]
+    let array = [1, 2, 3, 4, 6, 7]  //[3,4,5,7,8]  -> n(n+1)/2
+    //the sum of all the elements of that array - sum 
+    //sum till 1 to 7 
     let sumOfNumbers = 0
     for (let i = 0; i < array.length; i++) {
         sumOfNumbers = sumOfNumbers + array[i]
     }
     let first = array[array.length - 1]
     let second = array[0] - 1
-    let firstSum = first * (first + 1) / 2
-    let secondSum = second * (second + 1) / 2
+    let firstSum = first * (first + 1) / 2  // -> n(n+1)/2
+    let secondSum = second * (second + 1) / 2   // -> n(n+1)/2
     let total = firstSum - secondSum
     let missingNumber = total - sumOfNumbers
     res.send(`The missing number is ${missingNumber}`)
@@ -22,14 +56,14 @@ router.get('/test-me', function (req, res) {  //question number 1
 
 
 router.get('/test-you', function (req, res) {   //Question number - 2
-    let array = [33, 34, 35, 37, 38]
+    let array = [33, 34, 35, 36, 38]  //logic -> sum from (1-38)-(1-32) => (33-38)
     let sumOfNumbers = 0
     for (let i = 0; i < array.length; i++) {
         sumOfNumbers = sumOfNumbers + array[i]
     }
-    let first = array[array.length - 1]
+    let first = array[array.length - 1]  //last element 
     let second = array[0] - 1
-    let firstSum = first * (first + 1) / 2
+    let firstSum = first * (first + 1) / 2   //n(n+1)/2
     let secondSum = second * (second + 1) / 2
     let total = firstSum - secondSum
     let missingNumber = total - sumOfNumbers
@@ -37,6 +71,16 @@ router.get('/test-you', function (req, res) {   //Question number - 2
 })
 
 
-module.exports = router
 
+router.post('/players', function (req, res) {
+    let details = req.body
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].name == details.name) {
+            return res.status(200).send(`This player details are already present`)
+        }
+    }
+    array.push(details)
+    return res.send(array)
+})
 
+module.exports = routerd
