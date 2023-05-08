37d0652bde374940413f83e49dc3c5b4d3735117
Rahul <rahulmishra4317@gmial.com>
2023-05-09 01:20:35 +0530

    middleware amount deduction wala code and assignment

--- src/middlewares/commonMiddleware.js
+++ src/middlewares/commonMiddleware.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require("mongoose")
 const userModel = require("../models/userModel")
 const productModel = require('../models/productModel')
+const { response } = require("express")
 
 
 const isValid= function ( req, res, next) {
@@ -15,5 +16,43 @@ const isValid= function ( req, res, next) {
    }
 }
 
+const validId = function(req,res,next) {
+    const product = req.body.productId
+    const user = req.body.userId
+    if(!mongoose.isValidObjectId(product)) {
+        res.send({error:"Invalid product Id"})
+    }
+    else if(!mongoose.isValidObjectId(user)){
+        res.send({error:"invalid product Id"})
+    }
+    else { 
+        next()
+    }
+}
 
+const exist = async function(req,res,next) {
+    const product = req.body.productId
+    const user = req.body.userId
+    const userExist = await userModel.findById(user)
+    const productExist = await userModel.findById(product)
+
+    if(!userExist  || !productExist) {
+        if(!userExist) {
+            res.send({error : "User with this Id doesn't exist"})
+        }
+        else {
+            res.send({error : "product with this ID doesn't exist"})
+        }
+
+    }
+    else {
+        next()
+    }
+
+}
+
+
+
+module.exports.validId=validId
 module.exports.isValid= isValid
+module.exports.exist=exist
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const bodyParser = require('body-parser')
+const orderController = require('../controllers/orderController')
+const productController = require('../controllers/productController')
 const userController = require('../controllers/userController')
-const bookController = require('../controllers/bookControllers')
+
+router.post('/createProduct',productController.createProduct)
 
 
 
 
 
-router.post('/createBook',bookController.createBook)  //first problem done 
-router.post('/bookList',bookController.bookList)
-router.post('/getBooksByYear',bookController.getBooksByYear)
-router.post('/getParticularBook',bookController.getParticularBooks)
-router.get('/getXINRBooks',bookController.getXINRBooks)
-router.post('/getRandomBooks',bookController.getRandomBooks)
-router.get('/bookPrice', bookController.bookPrice)
+
 module.exports = router
